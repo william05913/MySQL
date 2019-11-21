765bca83fcdf29d8fb45480a91278c3c51a95e16
william05913 <william29amaya@gmail.com>
2019-11-20 23:49:42 -0600

    se le cambio el nombre al tolbar

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -77,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setTitleMargin(0, 0, 0, 0);
         toolbar.setSubtitle("CRUD MySQL 2019");
         toolbar.setSubtitleTextColor(getResources().getColor(R.color.mycolor));
-        toolbar.setTitle("William Ortiz");
+        toolbar.setTitle("WILLIAM ORTIZ");
         setSupportActionBar(toolbar);
 
         ///y esto para pantalla completa (oculta incluso la barra de estado)
